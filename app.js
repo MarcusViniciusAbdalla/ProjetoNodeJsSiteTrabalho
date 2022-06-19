f48582d606960fc223313de256d4620408463653
Marcus Vinicius Abdalla <93200321+MarcusViniciusAbdalla@users.noreply.github.com>
2022-06-19 16:15:31 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 const db = require('./db/connection');
+
+
 
 const PORT = 3000;
 
 app.listen(PORT , function(){
     console.log(`O Express está rodando na porta ${PORT}`);
 });
+
+//body parser
+app.use(bodyParser.urlencoded({ extended: false }));
 
 //db connection
 db
@@ -23,6 +29,9 @@ db
 app.get('/' , (req, res)=>{
     res.send('Está funcionando' )
 });
+
+//jobs routes
+app.use('/jobs' , require('./routes/jobs'));
 
 
 
