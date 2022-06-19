b2ae50811ce094e6756901518ae03db5724e2b10
Marcus Vinicius Abdalla <93200321+MarcusViniciusAbdalla@users.noreply.github.com>
2022-06-19 12:27:37 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
-
 const app = express();
+const db = require('./db/connection');
 
 const PORT = 3000;
 
@@ -8,6 +8,18 @@ app.listen(PORT , function(){
     console.log(`O Express está rodando na porta ${PORT}`);
 });
 
+//db connection
+db
+    .authenticate()
+    .then(() => {
+        console.log("Conectou ao Banco de Dados com Sucesso")
+    })
+    .catch(err => {
+        console.log("Ocorreu um erro ao conectar" , err)
+    });
+
+
+//routes
 app.get('/' , (req, res)=>{
     res.send('Está funcionando' )
 });
